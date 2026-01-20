fac035d7dccd24fe7fb37c545fde9603777dba67
Mateus Paulino <mateusgpaulino@gmail.com>
2026-01-20 13:09:40 -0300

    implemntando o Open/Closed Principle (OCP)

---
+++ app-etl/src/extrator/Arquivo.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Extrator;
+
+class Arquivo
+{
+    private $dados = [];
+
+    public function setDados(string $nome, string $cpf, string $email): void
+    {
+        array_push($this->dados, [
+            "nome" => iconv("ISO-8859-1", "UTF-8", $nome),
+            "cpf" => $cpf,
+            "email" => $email
+        ]);
+    }
+
+    public function getDados(): array
+    {
+        return $this->dados;
+    }
+}
